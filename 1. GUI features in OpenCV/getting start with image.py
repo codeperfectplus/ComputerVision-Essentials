26dcdad015bb669dd4c9a84394acd8680e6e57d1
codeperfectplus <deepak008@live.com>
2020-09-12 13:04:19 +0530

    :hammer: improved some fucntionns

--- 1. GUI features in OpenCV/getting start with image.py
+++ 1. GUI features in OpenCV/getting start with image.py
@@ -1,7 +1,7 @@
 import cv2
 
 #load an color image in grayscale color
-img = cv2.imread('../Media/sample.jpeg',0)
+img = cv2.imread('./Media/sample.jpeg',0)
 
 cv2.imshow('image', img)
 k = cv2.waitKey(0) & 0xFF
@@ -10,5 +10,5 @@ if k == 27:
     cv2.destroyAllWindows()
 # s key to save and exit
 elif k == ord('s'):
-    cv2.imwrite("../Media/sample2.jpeg", img)
+    cv2.imwrite("./Media/sample2.jpeg", img)    
     cv2.destroyAllWindows()
--- 1. GUI features in OpenCV/saving Video from camera.py
+++ 1. GUI features in OpenCV/saving Video from camera.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
 
 #define codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter("../Media/output.avi", fourcc, 20.0, (640,480))
+out = cv2.VideoWriter("./Media/output.avi", fourcc, 20.0, (640,480))
 
 while(cap.isOpened()):
     ret, frame = cap.read()
